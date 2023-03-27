b661e92d6d1a3647b51d7a76b3a7dccda0f8da7e
John Hunt <john.hunt@economicmodeling.com>
2023-03-27 15:07:19 -0700

    disable format on save, enable intelephense file associations

--- lsp/config/intelephense.lua
+++ lsp/config/intelephense.lua
@@ -1,10 +1,16 @@
-local util = require 'lspconfig.util'
-
 return {
---  ["intelephense"] = {
---    files = {
---      associations = { "*.php", "*.inc" }
---    },
---    root_dir = util.root_pattern(".git"),
---  }
+  filetypes = {
+    "php",
+    "inc"
+  },
+  settings = {
+    intelephense = {
+      files = {
+        associations = {
+          "*.php",
+          "*.inc"
+        }
+      }
+    }
+  }
 }
